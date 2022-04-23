6084c1665528074edbf0fb12a1273ec8d4bda6bb
Carmen Tsang <carmtsang89@hotmail.com>
2022-04-23 22:46:38 +0000

    made breed fetcher dynamic to command line

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,16 @@
 const request = require('request');
 
-request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
+const args = process.argv.slice(2);
+const breed = args[0];
+
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
   if (error) {
     console.log('error:', error)}; // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log('body:', body);
+
   const data = JSON.parse(body);
-  console.log(data);
+  console.log(data[0].description)
   
+  console.log(typeof data)
 
 });
