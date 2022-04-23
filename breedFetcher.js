30aeda217020f7b35b6dd8118f3310149609c566
Carmen Tsang <carmtsang89@hotmail.com>
2022-04-23 23:46:35 +0000

    added index.js, refractored breedFetcher and made it a function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,19 @@
 const request = require('request');
 
-const args = process.argv.slice(2);
-const breed = args[0];
-
-
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
-  } else {
-    const data = JSON.parse(body);
-  
-    if (!data[0].description) {
-      console.log(`We do not have any information on ${breed} cats`)
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null); // Print the error if one occurred
     } else {
-      console.log(data[0].description) //print discription of the cat.
-    }
+      const data = JSON.parse(body);
+    
+      if (!data[0]) {
+        return callback(`No information on ${breedName} cats found`, null)
+      }
 
-  }; 
-  
-});
+      return callback(null, data[0].description) //print discription of the cat.
+    }; 
+  })
+}
+
+module.exports = { fetchBreedDescription };
